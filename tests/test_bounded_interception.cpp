93b18c45c916062ac1bbf86e964bb2f44f8f9b34
Iftach Naftaly <iftahnaf@gmail.com>
2024-01-14 17:33:22 +0000

    feat: adding bounded interception tests

---
+++ tests/test_bounded_interception.cpp
@@ -0,0 +1,32 @@
+#include "../include/bounded_interception.hpp"
+#include <gtest/gtest.h>
+
+TEST(BoundedInterceptionTest, TgoCalculation) {
+    BoundedInterception bi;
+
+    Eigen::Vector3d r, v;
+    r << 0.0, 0.0, 110.0;
+    v << 0.0, 0.0, -10.0;
+
+    double actual_tgo = bi.bounded_interception_tgo(r, v);
+
+    EXPECT_GT(actual_tgo, 0.0);
+}
+
+TEST(BoundedInterceptionTest, ControllerCalculation) {
+    BoundedInterception bi;
+
+    Eigen::Vector3d r, v;
+    r << 0.0, 0.0, 110.0;
+    v << 0.0, 0.0, -10.0;
+
+    double tgo = bi.bounded_interception_tgo(r, v);
+    Eigen::Vector3d actual_controller = bi.bounded_interception_controller(r, v, tgo);
+
+    EXPECT_LE(actual_controller.norm(), bi.rho_u);
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
